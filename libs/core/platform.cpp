43d89864885673bd0f13be9a192bd8715ee36125
Sébastien NEDJAR <nedseb@goldaire.com>
2018-04-13 15:59:46 +0200

    Ajout des fichiers spécifiques à la cible stm32

---
+++ libs/core/platform.cpp
@@ -0,0 +1,19 @@
+#include "pxt.h"
+
+namespace pxt {
+
+static void initRandomSeed() {
+    int seed = 0xC0DA1;
+    seedRandom(seed);
+}
+
+static void initSwdPins() {
+}
+
+
+void platform_init() {
+    initSwdPins();
+    initRandomSeed();
+}
+
+}
