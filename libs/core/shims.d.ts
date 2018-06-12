b06fd5a8dac8543fd670863c2e703e8cb5d3ea2d
Sébastien NEDJAR <nedseb@goldaire.com>
2018-06-12 14:20:32 +0200

    Add library servo

--- libs/core/shims.d.ts
+++ libs/core/shims.d.ts
@@ -89,8 +89,22 @@ declare interface DigitalPin {
     //% blockNamespace=pins
     //% pin.fieldEditor="gridpicker"
     //% pin.fieldOptions.width=220
-    //% pin.fieldOptions.columns=4 shim=DigitalPinMethods::onPulsed
+    //% pin.fieldOptions.columns=4
+    //% parts="slideswitch" trackArgs=0
+    //% deprecated=1 hidden=1 shim=DigitalPinMethods::onPulsed
     onPulsed(pulse: PulseValue, body: () => void): void;
+
+    /**
+     * Register code to run when a pin event occurs. 
+     */
+    //% help=pins/on-event weight=20 blockGap=8
+    //% blockId=pinsonevent block="on|pin %pin|%event"
+    //% blockNamespace=pins
+    //% pin.fieldEditor="gridpicker"
+    //% pin.fieldOptions.width=220
+    //% pin.fieldOptions.columns=4
+    //% parts="slideswitch" trackArgs=0 shim=DigitalPinMethods::onEvent
+    onEvent(event: PinEvent, body: () => void): void;
 
     /**
      * Return the duration of a pulse in microseconds
@@ -98,7 +112,7 @@ declare interface DigitalPin {
      * @param value the value of the pulse (default high)
      * @param maximum duration in micro-seconds
      */
-    //% blockId="pins_pulse_in" block="pulse in (µs)|pin %name|pulsed %value"
+    //% blockId="pins_pulse_in" block="pulse in (µs)|pin %name|pulsed %high||timeout %maxDuration (us)"
     //% weight=18 blockGap=8
     //% help="pins/pulse-in"
     //% blockNamespace=pins
@@ -148,16 +162,17 @@ declare interface PwmOnlyPin {
      * rotation servo, this will set the speed of the servo (with ``0`` being full-speed in one
      * direction, ``180`` being full speed in the other, and a value near ``90`` being no movement).
      * @param name pin to write to
-     * @param value angle or rotation speed, eg:180,90,0
+     * @param value angle or rotation speed
      */
     //% help=pins/servo-write weight=41 group="Servo"
-    //% blockId=device_set_servo_pin block="servo write|pin %name|to %value" blockGap=8
+    //% blockId=device_set_servo_pin block="servo write|pin %name|to %value=protractorPicker" blockGap=8
     //% parts=microservo trackArgs=0
     //% blockNamespace=pins
     //% name.fieldEditor="gridpicker"
     //% name.fieldOptions.width=220
-    //% name.fieldOptions.columns=4 shim=PwmOnlyPinMethods::servoWrite
-    servoWrite(value: int32): void;
+    //% name.fieldOptions.columns=4
+    //% value.defl=90 shim=PwmOnlyPinMethods::servoWrite
+    servoWrite(value?: int32): void;
 
     /**
      * Set the pin for PWM analog output, make the period be 20 ms, and set the pulse width.
@@ -219,6 +234,39 @@ declare namespace pins {
      */
     //% repeat.defl=0 shim=pins::i2cWriteBuffer
     function i2cWriteBuffer(address: int32, buf: Buffer, repeat?: boolean): int32;
+}
+declare namespace pins {
+
+    /**
+     * Write to the SPI slave and return the response
+     * @param value Data to be sent to the SPI slave
+     */
+    //% help=pins/spi-write weight=5 advanced=true
+    //% blockId=spi_write block="spi write %value" shim=pins::spiWrite
+    function spiWrite(value: int32): int32;
+
+    /**
+     * Writes a given command to SPI bus, and afterwards reads the response.
+     */
+    //% help=pins/spi-transfer weight=4 advanced=true
+    //% blockId=spi_transfer block="spi transfer %command into %response" shim=pins::spiTransfer
+    function spiTransfer(command: Buffer, response: Buffer): void;
+
+    /**
+     * Sets the SPI frequency
+     * @param frequency the clock frequency, eg: 1000000
+     */
+    //% help=pins/spi-frequency weight=4 advanced=true
+    //% blockId=spi_frequency block="spi frequency %frequency" shim=pins::spiFrequency
+    function spiFrequency(frequency: int32): void;
+
+    /**
+     * Sets the SPI mode and bits
+     * @param mode the mode, eg: 3
+     */
+    //% help=pins/spi-mode weight=3 advanced=true
+    //% blockId=spi_mode block="spi mode %mode" shim=pins::spiMode
+    function spiMode(mode: int32): void;
 }
 
 // Auto-generated. Do not edit. Really.
