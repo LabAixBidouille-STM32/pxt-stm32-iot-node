fc7951cfee6e1c7541fef0895019e4f089f50d0e
Sébastien NEDJAR <nedseb@goldaire.com>
2018-07-04 21:41:34 +0200

    Add serial logging

--- libs/core/platform.cpp
+++ libs/core/platform.cpp
@@ -7,17 +7,23 @@ static void initRandomSeed() {
     seedRandom(seed);
 }
 
-static void initSwdPins() {
+static codal::_mbed::Serial *serial;
+void platformSendSerial(const char *data, int len) {
+    if (!serial) {
+        serial = new codal::_mbed::Serial(SERIAL_TX, SERIAL_RX);
+        serial->baud(9600);
+    }
+    serial->send((uint8_t*)data, len);
 }
 
 
 void platform_init() {
-    initSwdPins();
     initRandomSeed();
+    setSendToUART(platformSendSerial);
+}
+
 }
 
 void cpu_clock_init() {
-    // missing in Codal      
-}
-
+    
 }
