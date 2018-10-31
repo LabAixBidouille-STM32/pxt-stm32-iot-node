9484d3b88a7361bcaaf93ec6241351a4d9c5b7ad
Sébastien NEDJAR <nedseb@goldaire.com>
2018-10-31 17:18:22 +0200

    Update blutooth library

--- libs/bluetooth/bluetooth.cpp
+++ libs/bluetooth/bluetooth.cpp
@@ -50,7 +50,7 @@ namespace bluetooth {
     //% help=bluetooth/start-temperature-service
     //% blockId=bluetooth_start_temperature_service block="bluetooth temperature service" blockGap=8
     //% parts="bluetooth" weight=86
-    void startTemperatureService() {            
+    void startTemperatureService() {        
     }
 
     /**
@@ -60,7 +60,55 @@ namespace bluetooth {
     //% blockId=bluetooth_start_magnetometer_service block="bluetooth magnetometer service"
     //% parts="bluetooth" weight=85
     void startMagnetometerService() {    
-    }  
+    }
+
+    /**
+    *  Starts the Bluetooth UART service
+    */
+    //% help=bluetooth/start-uart-service
+    //% blockId=bluetooth_start_uart_service block="bluetooth uart service"
+    //% parts="bluetooth" advanced=true
+    void startUartService() {
+    }
+    
+    //%
+    void uartWriteString(String data) {
+    }    
+
+    //%
+    String uartReadUntil(String del) {
+    }    
+
+
+    /**
+    * Sends a buffer of data via Bluetooth UART
+    */
+    //%
+    void uartWriteBuffer(Buffer buffer) {
+    }
+
+    /**
+    * Reads buffered UART data into a buffer
+    */
+    //%
+    Buffer uartReadBuffer() {
+        startUartService();
+        int bytes = uart->rxBufferedSize();
+        auto buffer = mkBuffer(NULL, bytes);
+        int read = uart->read(buffer->data, buffer->length);
+        // read failed
+        if (read < 0) {
+            decrRC(buffer);
+            return mkBuffer(NULL, 0);
+        }
+        // could not fill the buffer
+        if (read != buffer->length) {
+            auto tmp = mkBuffer(buffer->data, read); 
+            decrRC(buffer); 
+            buffer = tmp;
+        }
+        return buffer;
+    }
 
     /**
     * Registers an event to be fired when one of the delimiter is matched.
@@ -68,7 +116,7 @@ namespace bluetooth {
     */
     //% help=bluetooth/on-uart-data-received
     //% weight=18 blockId=bluetooth_on_data_received block="bluetooth|on data received %delimiters=serial_delimiter_conv"
-    void onUartDataReceived(StringData* delimiters, Action body) {
+    void onUartDataReceived(String delimiters, Action body) {
     }
 
     /**
@@ -91,6 +139,29 @@ namespace bluetooth {
     void onBluetoothDisconnected(Action body) {
     } 
 
+    const int8_t CALIBRATED_POWERS[] = {-49, -37, -33, -28, -25, -20, -15, -10};
+    /**
+    * Advertise an Eddystone URL
+	* @param url the url to transmit. Must be no longer than the supported eddystone url length, eg: "https://makecode.com"
+	* @param power power level between 0 and 7, eg: 7
+    * @param connectable true to keep bluetooth connectable for other services, false otherwise.
+    */
+    //% blockId=eddystone_advertise_url block="bluetooth advertise url %url|with power %power|connectable %connectable"
+    //% parts=bluetooth weight=11 blockGap=8
+    //% help=bluetooth/advertise-url blockExternalInputs=1
+    void advertiseUrl(String url, int power, bool connectable) {
+    }
+
+    /**
+    * Advertise an Eddystone UID
+	* @param nsAndInstance 16 bytes buffer of namespace (bytes 0-9) and instance (bytes 10-15)
+	* @param power power level between 0 and 7, eg: 7
+    * @param connectable true to keep bluetooth connectable for other services, false otherwise.
+    */
+    //% parts=bluetooth weight=12 advanced=true
+    void advertiseUidBuffer(Buffer nsAndInstance, int power, bool connectable) {
+    }
+
     /**
     * Sets the bluetooth transmit power between 0 (minimal) and 7 (maximum).
     * @param power power level between 0 (minimal) and 7 (maximum), eg: 7.
@@ -99,4 +170,13 @@ namespace bluetooth {
     //% blockId=bluetooth_settransmitpower block="bluetooth set transmit power %power"
     void setTransmitPower(int power) {
     }
+
+    /**
+    * Stops advertising Eddystone end points
+    */
+    //% blockId=eddystone_stop_advertising block="bluetooth stop advertising"
+    //% parts=bluetooth weight=10
+    //% help=bluetooth/stop-advertising advanced=true
+    void stopAdvertising() {
+    } 
 }
