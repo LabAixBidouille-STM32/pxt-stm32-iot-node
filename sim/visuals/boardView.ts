7cc0badebfe0ae83198b556e9a67fda2c0dbfa0a
Sébastien NEDJAR <nedseb@goldaire.com>
2018-12-03 21:34:10 +0200

    update pxt

--- sim/visuals/boardView.ts
+++ sim/visuals/boardView.ts
@@ -10,6 +10,7 @@ namespace pxsim.visuals {
             runtime: runtime,
             theme: visuals.randomTheme(),
             visualDef: opts.visual as BoardImageDefinition,
+            boardDef: opts.boardDef,
             disableTilt: false,
             wireframe: opts.wireframe
         });
