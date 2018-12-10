db1f95118db369283e91f3bf464f571a4d532469
Sébastien NEDJAR <nedseb@goldaire.com>
2018-12-10 10:51:31 +0200

    Initialize correctly the second I2C

--- libs/core/platform.cpp
+++ libs/core/platform.cpp
@@ -1,20 +1,22 @@
 #include "pxt.h"
 #include "stm32l4xxSimpleSerial.h"
+#include "stm32l4xxI2C.h"
 
 namespace pxt {
+
+static codal::STM32L4xxSimpleSerial *serial;
+static codal::STM32L4xxI2C *i2c2;
 
 static void initRandomSeed() {
     int seed = 0xC0DA1;
     seedRandom(seed);
 }
 
-static codal::SimpleSerial *serial;
-
 void platformSendSerial(const char *data, int len) {
     
     if (!serial) {
         serial = new STM32L4xxSimpleSerial(*LOOKUP_PIN(RX), *LOOKUP_PIN(TX)) ;
-        
+        serial->init();
     }
     serial->send((uint8_t*)data, len);
 }
@@ -22,6 +24,9 @@ void platformSendSerial(const char *data, int len) {
 void platform_init() {
     initRandomSeed();
     setSendToUART(platformSendSerial);
+    i2c2 = new STM32L4xxI2C(*LOOKUP_PIN(ACCELEROMETER_SDA), *LOOKUP_PIN(ACCELEROMETER_SCL));
+    i2c2->init();
+    codal::default_i2c_sensors_bus = i2c2->getHandle();
 }
 
 }
